391f625a90080ef87632832df6c26119ad66fb4a
lovasoa <pere.jobs@gmail.com>
2021-03-22 17:17:12 +0100

    dependency script

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-sudo dnf install coin-or-Osi coin-or-Osi-devel
-
+set -x
+if test -x "$(which apt-get)"; then
+  sudo apt-get install coinor-libosi-dev coinor-libosi-doc coinor-libosi1v5
+elif test -x "$(which dnf)"; then
+  sudo dnf install coin-or-Osi coin-or-Osi-devel
+elif test -x "$(which brew)"; then
+  brew install osi
+else
+  echo "system not supported"
+  exit 1
+fi
