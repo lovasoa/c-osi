2847831875c4cda96c0c7db86087e6228ce2c694
lovasoa <pere.jobs@gmail.com>
2021-03-22 21:23:15 +0100

    add glpk to dependencies

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,7 +2,7 @@
 
 set -x
 if test -x "$(which apt-get)"; then
-  sudo apt-get install coinor-libosi-dev coinor-libosi-doc coinor-libosi1v5
+  sudo apt-get install coinor-libosi-dev coinor-libosi-doc coinor-libosi1v5 libglpk-dev
 elif test -x "$(which dnf)"; then
   sudo dnf install coin-or-Osi coin-or-Osi-devel
 elif test -x "$(which brew)"; then
